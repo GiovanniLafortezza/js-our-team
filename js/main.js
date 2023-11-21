f167c43cc444487f2d7d24c16fbc41aa82f26f37
GiovanniLafortezza <recordeknot388@gmail.com>
2023-11-21 15:52:01 +0100

    bonus ended

--- js/main.js
+++ js/main.js
@@ -5,46 +5,70 @@ let ourTeam = [
         name : "Wayne",
         surname : "Barnett",
         role : "Founder & CEO",
-        image : "wayne-barnett-founder-ceo.jpg"
+        image : "./img/wayne-barnett-founder-ceo.jpg"
     },
     {
         name : "Angela",
         surname : "Caroll",
         role : "Chief Editor",
-        image : "angela-caroll-chief-editor.jpg"
+        image : "./img/angela-caroll-chief-editor.jpg"
     },
     {
         name : "Walter",
         surname : "Gordon",
-        role : "Office Menager",
-        image : "walter-gordon-office-menager.jpg"
+        role : "Office Manager",
+        image : "./img/walter-gordon-office-manager.jpg"
     },
     {
         name : "Angela",
         surname : "Lopez",
-        role : "Social Media Menager",
-        image : "angela-lopez-social-media-menager.jpg"
+        role : "Social Media Manager",
+        image : "./img/angela-lopez-social-media-manager.jpg"
     },
     {
         name : "Scott",
         surname : "Estrada",
         role : "Developer",
-        image : "scott-estrada-developer.jpg"
+        image : "./img/scott-estrada-developer.jpg"
     },
     {
         name : "Barbara",
         surname : "Ramos",
         role : "Graphic Designer",
-        image : "barbara-ramos-graphic-designer.jpg"
+        image : "./img/barbara-ramos-graphic-designer.jpg"
     }
 
 ];
 
-printArrayTeam(ourTeam);
+for (let i = 0; i < ourTeam.length; i++) {
+
+    let member = ourTeam[i];
+
+    console.log("Il membro n° " + (i + 1) + " è:");
+    printObject(member);
+
+
+    let memberCard =
+            `<div class="col-4">
+                <div class="card rounded-0 border-0 shadow w-75">
+                    <img src="${member.image}" class="img-fluid card-img-top rounded-0" alt="team-member">
+                    <div class="card-body text-center">
+                        <h4 class="card-title fw-bold">${member.name} \t ${member.surname}</h4>
+                        <p class="card-text fs-5">${member.role}</p>
+                    </div>
+                </div>
+            </div>`;
+
+    let container = document.getElementById("container_card");
+    container.insertAdjacentHTML('beforeend', memberCard);
+}
+
 
 
 // ----------------- FUNZIONI -------------------------------
 
+
+// ---------------- stampa un oggetto -----------------------
 function printObject(object) {
     let string = "";
 
@@ -60,7 +84,7 @@ function printObject(object) {
 
 }
 
-
+// ---------------- stampa un array di oggetti -----------------------
 function printArrayTeam(arrayTeam) {
     for (let i = 0; i < arrayTeam.length; i++) {
 
@@ -70,3 +94,5 @@ function printArrayTeam(arrayTeam) {
         printObject(member);
     }
 }
+
+
